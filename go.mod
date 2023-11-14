41a4bb915777a18bea99950d8c603f8c20a4703b
Divyanshu Gupta <divyanshu.gupta01@indiamart.com>
2023-11-14 17:38:01 +0530

    Adding starter code

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dg222599/go-db
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+	// "os"
+	// "encoding/json"
+)
+
+func main(){
+	 
+	fmt.Println("Starting the DB build")
+}
